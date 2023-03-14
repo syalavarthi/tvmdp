d07c2e0e4778d478975a6a4f5dfa91952b7c6b9b
Prince Takkar <ptakkar@marvell.com>
2023-03-13 22:09:31 -0700

    Implement TVMDP fast path and slow path fucntions
    Implemented TVMDP fast-path ands slow-path APIs defined by the spec
    
    tvmdp_configure: initilaize internal tvmdp resources
    tvmdp_close(): Release the allocated memory.
    tvmdp_model_load: load the model artifacts and initialize model handles
    tvmdp_model_metadata_get: retrieve model metadata
    tvmdp_model_unload: unload the model
    tvmdp_model_run: run the inference using TVM runtime
    
    IPBUSW-28609
    IPBUSW-28611
    IPBUSW-27551
    
    Signed-off-by: Prince Takkar <ptakkar@marvell.com>
    Change-Id: Ifaab1008e3d27f970aefb45f46b476206715bf82
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/cdk/tvm/tvmdp/+/99086
    Reviewed-by: Srikanth Yalavarthi <syalavarthi@marvell.com>
    Tested-by: Srikanth Yalavarthi <syalavarthi@marvell.com>

--- src/tvmdp.cpp
+++ src/tvmdp.cpp
@@ -4,10 +4,22 @@
 
 #include <tvmdp.h>
 
-#include <cstdint>
+#include <cstdlib>
 #include <iostream>
 
+#include <linux/limits.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#include <jansson.h>
 #include <tvm/runtime/module.h>
+#include <tvm/runtime/registry.h>
+
+/* Shared memory file descriptor name */
+#define ML_MODEL_SHMFD_NAME "tvmdp_shmfd"
+
+/* Shared memory file descriptor path */
+#define ML_MODEL_SHMFD_PATH "/proc/%d/fd/%d"
 
 namespace tvmdp
 {
@@ -46,7 +58,32 @@ tvmdp_hello(void)
 int
 tvmdp_configure(uint16_t nb_models)
 {
-	(void)nb_models;
+	/* Check for configuration status */
+	if (data.model != nullptr) {
+		std::cout << "Device already configured" << std::endl;
+		return 0;
+	}
+
+	/* Check for nb_models value */
+	if (nb_models == 0) {
+		std::cerr << "Invalid arguments, nb_models=" << nb_models << std::endl;
+		return -EINVAL;
+	}
+
+	/* Allocate memory for model data */
+	data.model = reinterpret_cast<struct tvmdp_model_data *>(
+		malloc(nb_models * sizeof(struct tvmdp_model_data)));
+	if (data.model == nullptr) {
+		std::cerr << "Memory allocation failed for data" << std::endl;
+		return -ENOMEM;
+	}
+	data.nb_models = nb_models;
+
+	/* Reset memory */
+	memset(data.model, 0, sizeof(struct tvmdp_model_data) * nb_models);
+
+	/* Initialize the device */
+	data.device = DLDevice{kDLCPU, 0};
 
 	return 0;
 }
@@ -54,6 +91,27 @@ tvmdp_configure(uint16_t nb_models)
 int
 tvmdp_close(void)
 {
+	uint16_t i;
+
+	/* Check configuration status */
+	if (data.model == nullptr) {
+		std::cerr << "TVMDP not configured" << std::endl;
+		return -EINVAL;
+	}
+
+	/* Check for active models */
+	for (i = 0; i < data.nb_models; i++) {
+		if (data.model[i].module->defined()) {
+			std::cerr << "Model still in use, model_id = " << i << std::endl;
+			return -EINVAL;
+		}
+	}
+
+	/* Free memory */
+	free(data.model);
+
+	/* Reset the pointer */
+	data.model = NULL;
 
 	return 0;
 }
@@ -61,8 +119,64 @@ tvmdp_close(void)
 int
 tvmdp_model_load(uint16_t model_id, void *model_object)
 {
-	(void)model_id;
-	(void)model_object;
+	struct tvmdp_model_object *object;
+	tvm::runtime::Module module_so;
+	tvm::runtime::Module module_ge;
+
+	int device_type = kDLCPU;
+	char str[PATH_MAX];
+	int fd;
+
+	/* Check configuration status */
+	if (data.model == nullptr) {
+		std::cerr << "TVMDP not configured" << std::endl;
+		return -EINVAL;
+	}
+
+	/* Check if model is loaded */
+	if (data.model[model_id].module->defined()) {
+		std::cerr << "Model is already loaded, model_id = " << model_id << std::endl;
+		return -EINVAL;
+	}
+
+	/* Check for arguments */
+	if (model_object == nullptr) {
+		std::cerr << "Invalid arguments, model_object = NULL" << std::endl;
+		return -EINVAL;
+	}
+
+	/* Create Shared memory file descriptor */
+	snprintf(str, PATH_MAX, "%s_%d_%d", ML_MODEL_SHMFD_NAME, getpid(), model_id);
+	fd = memfd_create(str, 0);
+	if (fd < 0) {
+		std::cerr << "Failed to create shared memory descriptor object" << std::endl;
+		return -errno;
+	}
+
+	/* Writing buffer data to shared memory fd */
+	object = reinterpret_cast<struct tvmdp_model_object *>(model_object);
+	if (write(fd, object->so.addr, object->so.size) < 0) {
+		std::cerr << "Failed to write to shared memory object" << std::endl;
+		close(fd);
+		return -errno;
+	}
+
+	lseek(fd, SEEK_SET, 0);
+
+	/* Loading *.so file */
+	snprintf(str, PATH_MAX, ML_MODEL_SHMFD_PATH, getpid(), fd);
+	std::cout << "fd path: " << str << std::endl;
+	module_so = tvm::runtime::Module::LoadFromFile(str, "so");
+
+	/* Calling Graph executor */
+	module_ge = (*tvm::runtime::Registry::Get("tvm.graph_executor.create"))(
+		object->json.addr, module_so, device_type, data.device.device_id);
+
+	data.model[model_id].module = new tvm::runtime::Module(module_ge);
+
+	/* Copy model object structure */
+	memcpy(&data.model[model_id].object, object, sizeof(struct tvmdp_model_object));
+	data.model[model_id].model_id = model_id;
 
 	return 0;
 }
@@ -70,7 +184,23 @@ tvmdp_model_load(uint16_t model_id, void *model_object)
 int
 tvmdp_model_unload(uint16_t model_id)
 {
-	(void)model_id;
+	/* Check configuration status */
+	if (data.model == nullptr) {
+		std::cerr << "TVMDP not configured" << std::endl;
+		return -EINVAL;
+	}
+
+	/* Check if model is loaded */
+	if (data.model[model_id].module == NULL) {
+		std::cout << "Model is not loaded, model_id = " << model_id << std::endl;
+		return 0;
+	}
+
+	/* Delete module */
+	delete data.model[model_id].module;
+
+	/* Reset handle */
+	data.model[model_id].module = NULL;
 
 	return 0;
 }
@@ -78,8 +208,136 @@ tvmdp_model_unload(uint16_t model_id)
 int
 tvmdp_model_metadata_get(uint16_t model_id, void *metadata_addr)
 {
-	(void)model_id;
-	(void)metadata_addr;
+	tvm::runtime::Map<tvm::runtime::String, tvm::runtime::NDArray> Map;
+	tvm::runtime::ShapeTuple shape;
+	tvm::runtime::NDArray io_array;
+	tvm::runtime::Module *module;
+	TVMByteArray params;
+
+	json_error_t json_error;
+	json_t *json_parsed;
+	json_t *json_nodes;
+	json_t *json_array;
+	json_t *json_name;
+	json_t *json_op;
+
+	struct tvmdp_model_metadata *metadata;
+	struct tvmdp_model_object object;
+	DLDataType dtype;
+	int flag = 0;
+	int i;
+
+	/* Check configuration status */
+	if (data.model == nullptr) {
+		std::cerr << "TVMDP not configured" << std::endl;
+		return -EINVAL;
+	}
+
+	/* Check if model is loaded */
+	if (data.model[model_id].module == NULL) {
+		std::cout << "Model is not loaded, model_id = " << model_id << std::endl;
+		return 0;
+	}
+
+	object = data.model[model_id].object;
+	module = data.model[model_id].module;
+
+	if (!module->defined()) {
+		std::cout << "TVMDP Model Load either failed or not called" << std::endl;
+		return 0;
+	}
+
+	/* Load JSON */
+	json_parsed = json_loadb((const char *)object.json.addr, object.json.size, 0, &json_error);
+	if (!json_parsed) {
+		fprintf(stderr, "error: on line %d: %s\n", json_error.line, json_error.text);
+		return -1;
+	}
+
+	/* Loading params data */
+	params.data = reinterpret_cast<const char *>(object.params.addr);
+	params.size = object.params.size;
+	module->GetFunction("load_params")(params);
+
+	Map = (*tvm::runtime::Registry::Get("runtime.LoadParams"))(params);
+
+	json_nodes = json_object_get(json_parsed, "nodes");
+	if (json_nodes == NULL) {
+		std::cerr << "Failed parsing JSON nodes" << std::endl;
+		return -errno;
+	}
+
+	/* Get number of inputs */
+	metadata = reinterpret_cast<struct tvmdp_model_metadata *>(metadata_addr);
+	for (i = 0; i < ((int)json_array_size(json_nodes)); i++) {
+		json_array = json_array_get(json_nodes, i);
+		json_op = json_object_get(json_array, "op");
+
+		if (strcmp(json_string_value(json_op), "null") == 0) {
+			json_name = json_object_get(json_array, "name");
+			flag = 0;
+
+			for (auto &p : Map) {
+				if (strcmp(p.first.c_str(), json_string_value(json_name)) == 0) {
+					flag = 1;
+					break;
+				}
+			}
+
+			if (flag == 0) {
+				strcpy(metadata->input[metadata->model.num_input].name,
+				       json_string_value(json_name));
+				metadata->model.num_input++;
+				std::cout << "Input name: %s" << json_string_value(json_name)
+					  << std::endl;
+			}
+		}
+	}
+
+	std::cout << "Number of inputs: " << metadata->model.num_input << std::endl;
+
+	/* Get number of outputs */
+	metadata->model.num_output = module->GetFunction("get_num_outputs")();
+	std::cout << "Number of outputs: " << metadata->model.num_output << std::endl;
+
+	/* Set Model datatype */
+	dtype = DLDataType{kDLFloat, 32, 1};
+
+	/* Get input shape */
+	for (i = 0; i < metadata->model.num_input; i++) {
+		io_array = module->GetFunction("get_input")(metadata->input[i].name);
+		shape = io_array.Shape();
+		metadata->input[i].ndim = 0;
+
+		for (auto &p : shape) {
+			metadata->input[i].shape[metadata->input[i].ndim] = p;
+			metadata->input[i].ndim++;
+		}
+		/* Set device type and device id */
+		metadata->input[i].device = data.device;
+		metadata->input[i].datatype = dtype;
+		metadata->input[i].model_datatype = dtype;
+		metadata->input[i].scale = 1.0;
+		metadata->input[i].format = 0;
+	}
+
+	/* Get output shape */
+	for (i = 0; i < metadata->model.num_output; i++) {
+		io_array = module->GetFunction("get_output")(i);
+		shape = io_array.Shape();
+		metadata->output[i].ndim = 0;
+
+		for (auto &p : shape) {
+			metadata->output[i].shape[metadata->output[i].ndim] = p;
+			metadata->output[i].ndim++;
+		}
+		/* Set device type and device id */
+		metadata->output[i].device = data.device;
+		metadata->output[i].datatype = dtype;
+		metadata->output[i].model_datatype = dtype;
+		metadata->output[i].scale = 1.0;
+		metadata->output[i].format = 0;
+	}
 
 	return 0;
 }
@@ -88,11 +346,19 @@ int
 tvmdp_model_run(uint16_t model_id, int32_t num_input, DLTensor *input_tensor, int32_t num_output,
 		DLTensor *output_tensor)
 {
-	(void)model_id;
-	(void)num_input;
-	(void)input_tensor;
-	(void)num_output;
-	(void)output_tensor;
+	tvm::runtime::Module *module;
+
+	int i = 0;
+
+	module = data.model[model_id].module;
+
+	for (i = 0; i < num_input; i++)
+		module->GetFunction("set_input_zero_copy")(i, &input_tensor[i]);
+
+	for (i = 0; i < num_output; i++)
+		module->GetFunction("set_output_zero_copy")(i, &output_tensor[i]);
+
+	module->GetFunction("run")();
 
 	return 0;
 }
